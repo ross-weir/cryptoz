a5175423448426604ddd78f20fe3c8d1ca1d568e
ross-weir <29697678+ross-weir@users.noreply.github.com>
2024-12-27 14:01:23 +1100

    export ripemd128 hash fn

--- src/cryptoz.zig
+++ src/cryptoz.zig
@@ -3,10 +3,12 @@ pub const hash = struct {
     pub const Md4 = @import("hash/md4.zig").Md4;
     pub const Md5 = @import("hash/md5.zig").Md5;
     pub const Sha1 = @import("hash/sha1.zig").Sha1;
+    pub const Ripemd128 = @import("hash/ripemd.zig").Ripemd128;
 };
 
 test {
     _ = hash.Md4;
     _ = hash.Md5;
     _ = hash.Sha1;
+    _ = hash.Ripemd128;
 }
